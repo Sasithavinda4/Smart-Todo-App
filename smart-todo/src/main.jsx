5dbbe9a09472871908b5a3fe71b9472915743488
Sasith Avinda Abeysundara <121947470+Sasithavinda4@users.noreply.github.com>
2026-04-03 12:53:09 +0530

    main.jsx updated..

--- smart-todo/src/main.jsx
+++ smart-todo/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { TodoProvider } from './context/TodoContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// Wraps your whole app inside TodoProvider.. So, any component inside <App /> can access todos..
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <TodoProvider>
     <App />
-  </StrictMode>,
+  </TodoProvider>,
 )
